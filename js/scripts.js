06c700f772d8273ac733ab0cd25742d7abb4411c
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-08 14:38:18 -0700

    add final

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,25 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
+    console.log(person1Input)
+    console.log(person2Input)
+    console.log(animalInput)
+    console.log(exclamationInput)
+    console.log(verbInput)
+    console.log(nounInput)
+
+
     let madlibs = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
+   
+    console.log(madlibs);
 
     madlibs.forEach(function(madlib) {
-      $('#story').replaceWith('<ul>' + madlib + '</ul>');
-    })
+     
+      // $('.output').replaceWith('<ul>' + madlib + '</ul>');
+      $('.output').text(madlib);
+      console.log(madlib);
+    });
+    
 
     // $(".person1").text(person1Input);
     // $(".person2").text(person2Input);
@@ -21,7 +35,7 @@ $(document).ready(function() {
     // $(".verb").text(verbInput);
     // $(".noun").text(nounInput);
 
-    // $("#story").show();
+    $(".output").show();
 
     
   });
